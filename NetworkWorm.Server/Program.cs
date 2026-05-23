8f88a34f444310090dc361a7e1617bbfb2014333
MrtviDonec <143379123+MrtviDonec@users.noreply.github.com>
2026-05-23 15:56:11 +0300

    Update Program.cs

--- NetworkWorm.Server/Program.cs
+++ NetworkWorm.Server/Program.cs
@@ -1,13 +1,56 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using NetworkWorm.Server.Data;
 using NetworkWorm.Server.Hubs;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Добавляем контроллеры
 builder.Services.AddControllers();
 
 builder.Services.AddSignalR();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// JWT аутентификация
+var jwtSecret = builder.Configuration["JwtSettings:Secret"];
+if (string.IsNullOrEmpty(jwtSecret))
+{
+    jwtSecret = "super-secret-key-minimum-32-characters-for-jwt-security-12345";
+}
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSecret))
+        };
+
+        // Для SignalR - берем токен из query string
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                var accessToken = context.Request.Query["access_token"];
+                var path = context.HttpContext.Request.Path;
+                if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/learningHub"))
+                {
+                    context.Token = accessToken;
+                }
+                return Task.CompletedTask;
+            }
+        };
+    });
+
+builder.Services.AddAuthorization();
+builder.Services.AddScoped<NetworkWorm.Server.Services.AuthService>();
 
 builder.Services.AddCors(options =>
 {
@@ -23,12 +66,12 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 app.UseCors("AllowAll");
-
-app.MapControllers();
-
+app.UseAuthentication();  // ВАЖНО!
+app.UseAuthorization();
+app.MapControllers();     // ВАЖНО!
 app.MapHub<ChatHub>("/learningHub");
-app.MapGet("/", () => "SignalR ñåðâåð ðàáîòàåò!");
-app.MapGet("/test", () => "Ñåðâåð æèâ, áëÿ!");
+app.MapGet("/", () => "Марсияанин я собянин отзовись!");
+app.MapGet("/test", () => "Сервер выбрался из спячки хихихи ГОЙДААААААААААААААААА!");
 
 using (var scope = app.Services.CreateScope())
 {
