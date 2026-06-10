8ed1de9f0612e2acaf973b02431a89b1b5088791
MrtviDonec <143379123+MrtviDonec@users.noreply.github.com>
2026-06-10 20:54:21 +0300

    Add files via upload

---
+++ NetworkWorm.Server/Models/LabWork.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace NetworkWorm.Server.Models
+{
+    public class LabWork
+    {
+        public int Id { get; set; }
+        public int SectionId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Instructions { get; set; }
+        public object Steps { get; set; } // JSONB
+        public int TotalSteps { get; set; }
+        public int PassingSteps { get; set; }
+        public List<string> EquipmentList { get; set; }
+        public int EstimatedTimeHours { get; set; }
+        public int MaxScore { get; set; }
+        public bool IsRequired { get; set; }
+    }
+}
---
+++ NetworkWorm.Server/Models/SegmentationTask.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace NetworkWorm.Server.Models
+{
+    public class SegmentationTask
+    {
+        public int Id { get; set; }
+        public int SectionId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string NetworkAddress { get; set; }
+        public object Departments { get; set; } // JSONB
+        public object VlanRequirements { get; set; } // JSONB
+        public List<string> EquipmentList { get; set; }
+        public string SolutionSubnetMask { get; set; }
+        public string SolutionVlanAssignment { get; set; }
+        public string SolutionIpAllocation { get; set; }
+        public int MaxScore { get; set; }
+        public int DifficultyLevel { get; set; }
+        public object TaskSteps { get; set; } // JSONB
+        public int TotalSteps { get; set; }
+        public int PassingSteps { get; set; }
+    }
+}
